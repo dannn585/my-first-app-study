5febc15e1fe4081b939e02652b2da4d4205e7d37
ダン <ssrdn11@gmail.com>
2026-02-26 23:29:57 +0900

    見出しの装飾

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App(){
   return (
     <div>
-      <h1>自分専用のアプリ、始動します！</h1>
-      <p>ここに好きなものを作っていきます。</p>
+      <h1 className = "text-blue-50 text-center bg-gray-100 p-4 m-2 rounded-lg shadow- hover:text-blue-500">初のアプリ始動！</h1>
+      <p className="text-slate">ここに好きなものを作っていく</p>
     </div>
   )
 }
