1316c2f2ce6b63b3594584a08640af9d756a9199
ダン <ssrdn11@gmail.com>
2026-02-27 10:51:38 +0900

    デザイン変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App(){
   return (
     <div>
-      <h1 className = "text-blue-500 text-center bg-gray-100 border-red-300 accent-pink-500 p-4 m-2 rounded-lg shadow- hover:text-blue-800 font-bold">初のアプリ始動！</h1>
+      <h1 className = "text-blue-500 text-center text-lg bg-gray-100 border-red-300 accent-pink-500 p-4 m-2 rounded-lg shadow- hover:text-blue-800 font-bold">初のアプリ始動！</h1>
       <p className="text-slate font-bold">ここに好きなものを作っていく</p>
     </div>
   )
