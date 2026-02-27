0c9701a2d618cd8683e64b86587bdf285731e794
ダン <ssrdn11@gmail.com>
2026-02-27 11:38:24 +0900

    ヘッダーの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App(){
   return (
     <div>
       <h1 className = "text-blue-500 text-center text-lg bg-gray-100 border-red-300 accent-pink-500 p-4 m-2 rounded-lg shadow- hover:text-blue-800 font-bold">初のアプリ始動！</h1>
-      <p className="text-slate font-bold">ここに好きなものを作っていく</p>
+      <p className="text-slate-500 font-bold">ここに好きなものを作っていく</p>
     </div>
   )
 }
